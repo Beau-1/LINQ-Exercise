e522cd2c04e4c587b89f2054edebf33e949882d5
Beau <jamesbuchanan0528@gmail.com>
2022-12-20 14:16:33 -0600

    LINQ Ex 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace LINQ
+﻿
+
+List<string> names = new List<string>()
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    "Super Mario Bros.", "Batman", "Comand and Conqure", "Street Fighter", "Hunt:Showdown"
+};
+
+IEnumerable<string> sortedList = names.OrderBy(x => x.Length);
+
+
+foreach (var item in sortedList)
+{
+    Console.WriteLine(item);
 }
